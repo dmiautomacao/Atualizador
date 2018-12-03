2b0f1ca390bc4ff85957e6a2c7d97cad1fb585ed
Leonardo Freitas <lftecnologia@hotmail.com>
2018-12-03 09:17:17 -0200

    teste

---
+++ fusionAtualizer/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace fusionAtualizer
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.pb = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblPdv = new System.Windows.Forms.Label();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblRetaguarda = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb
+            // 
+            this.pb.Location = new System.Drawing.Point(44, 238);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(555, 24);
+            this.pb.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(40, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Atualização Online";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.SystemColors.WindowText;
+            this.lblStatus.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.White;
+            this.lblStatus.Location = new System.Drawing.Point(40, 56);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(84, 25);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Status: ";
+            // 
+            // lblPdv
+            // 
+            this.lblPdv.AutoSize = true;
+            this.lblPdv.BackColor = System.Drawing.SystemColors.WindowText;
+            this.lblPdv.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPdv.ForeColor = System.Drawing.Color.White;
+            this.lblPdv.Location = new System.Drawing.Point(40, 96);
+            this.lblPdv.Name = "lblPdv";
+            this.lblPdv.Size = new System.Drawing.Size(53, 25);
+            this.lblPdv.TabIndex = 6;
+            this.lblPdv.Text = "Pdv:";
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(477, 96);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(122, 87);
+            this.btnAtualizar.TabIndex = 7;
+            this.btnAtualizar.Text = "Atualizar Assincrono";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::fusionAtualizer.Properties.Resources.Dynamic_line_light_blue_background;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(647, 286);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblRetaguarda
+            // 
+            this.lblRetaguarda.AutoSize = true;
+            this.lblRetaguarda.BackColor = System.Drawing.SystemColors.WindowText;
+            this.lblRetaguarda.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRetaguarda.ForeColor = System.Drawing.Color.White;
+            this.lblRetaguarda.Location = new System.Drawing.Point(40, 139);
+            this.lblRetaguarda.Name = "lblRetaguarda";
+            this.lblRetaguarda.Size = new System.Drawing.Size(127, 25);
+            this.lblRetaguarda.TabIndex = 8;
+            this.lblRetaguarda.Text = "Retaguarda:";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 274);
+            this.Controls.Add(this.lblRetaguarda);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.lblPdv);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fusion Atualizer";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar pb;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblPdv;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Label lblRetaguarda;
+    }
+}
+
---
+++ fusionAtualizer/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+namespace fusionAtualizer
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+        private static Boolean OutDatedFusion()
+        {
+            try { 
+                string address = "https://dmiautomacao.000webhostapp.com/Fusion.zip";
+                HttpWebRequest fusionfile = (HttpWebRequest)WebRequest.Create(address);
+                HttpWebResponse fusionFileResponse = (HttpWebResponse)fusionfile.GetResponse();
+                fusionFileResponse.Close();
+                string path = System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "Fusion.zip";
+                DateTime localFileModifiedTime = File.GetLastWriteTime(path);
+                DateTime onlineFileModifiedTime = fusionFileResponse.LastModified;
+                return localFileModifiedTime >= onlineFileModifiedTime ? false : true;
+            }
+            catch (WebException e)
+            {
+                MessageBox.Show("Erro Ocorrido: " + e.Message + "\nStatus: " + e.Status);
+                return false;
+            }
+
+        }
+
+        private static Boolean OutDatedPdv()
+        {
+            try
+            {
+                string address = "https://dmiautomacao.000webhostapp.com/Pdv.zip";
+                System.Net.HttpWebRequest pdvfile = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(address);
+                System.Net.HttpWebResponse pdvFileResponse = (System.Net.HttpWebResponse)pdvfile.GetResponse();
+                pdvFileResponse.Close();
+
+                string path = System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "Pdv.zip";
+                DateTime localFileModifiedTime = File.GetLastWriteTime(path);
+                DateTime onlineFileModifiedTime = pdvFileResponse.LastModified;
+                return localFileModifiedTime >= onlineFileModifiedTime ? false : true;
+            }
+            catch (WebException e)
+            {
+                MessageBox.Show("Erro Ocorrido:\n " + e.Message + "\nStatus: " + e.Status);
+                return false;
+            }
+        }
+
+        private void FusionDownload()
+        {
+            string address = "https://dmiautomacao.000webhostapp.com/Fusion.zip";
+            System.Net.WebClient client = new System.Net.WebClient();
+            Uri uri = new Uri(address);
+            client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
+            client.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(DownloadFileFusionZip);
+            // Specify a progress notification handler.
+            client.DownloadProgressChanged += new System.Net.DownloadProgressChangedEventHandler(DownloadProgressFusionZip);
+            try { 
+                client.DownloadFileAsync(uri, System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "Fusion.zip");
+                while (client.IsBusy) { Application.DoEvents(); }
+                client.Dispose();
+            }
+            catch (WebException e)
+            {
+                MessageBox.Show("Erro Ocorrido: " + e.Message + "\nStatus: " + e.Status);
+            }
+        }
+
+        private void DownloadProgressFusionZip(object sender, DownloadProgressChangedEventArgs e)
+        {
+            pb.Value = e.ProgressPercentage;
+            lblRetaguarda.Text = "Status: Atualizando Fusion " + e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void DownloadFileFusionZip(object sender, AsyncCompletedEventArgs e)
+        {
+            UnzipFileFusion();
+        }
+
+        private void PdvDownload()
+        {
+            string address = "https://dmiautomacao.000webhostapp.com/Pdv.zip";
+            WebClient client = new WebClient();
+            Uri uri = new Uri(address);
+            client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
+            client.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(DownloadFilePdv);
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DownloadProgressPdv);
+            try { 
+                client.DownloadFileAsync(uri, System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "Pdv.zip");
+                while (client.IsBusy) { Application.DoEvents(); }
+                client.Dispose();
+            }
+            catch (WebException e)
+            {
+                MessageBox.Show("Erro Ocorrido:\n " + e.Message + "\nStatus: " + e.Status);
+            }
+        }
+
+        private void DownloadProgressPdv(object sender, DownloadProgressChangedEventArgs e)
+        {
+            pb.Value = e.ProgressPercentage;
+            lblPdv.Text = "Status: Atualizando PDV " + e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void DownloadFilePdv(object sender, AsyncCompletedEventArgs e)
+        {
+            UnzipFilePdv();
+        }
+
+        private void UnzipFileFusion()
+        {
+            string zipPath = System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "Fusion.zip";
+            string extractPath = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+
+            // Normalizes the path.
+            extractPath = Path.GetFullPath(extractPath);
+
+            if (!extractPath.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                extractPath += Path.DirectorySeparatorChar;
+            }
+
+            using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+            {
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    if (entry.FullName.EndsWith(".exe", StringComparison.OrdinalIgnoreCase))
+                    {
+                        // Gets the full path to ensure that relative segments are removed.
+                        string destinationPath = Path.GetFullPath(Path.Combine(extractPath, entry.FullName));
+
+                        // Ordinal match is safest, case-sensitive volumes can be mounted within volumes that
+                        // are case-insensitive.
+                        if (destinationPath.StartsWith(extractPath, StringComparison.Ordinal))
+                            entry.ExtractToFile(destinationPath);
+                    }
+                }
+            }
+        }
+
+        private void UnzipFilePdv()
+        {
+            string zipPath = System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "Pdv.zip";
+            string extractPath = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+
+            // Normalizes the path.
+            extractPath = Path.GetFullPath(extractPath);
+
+            if (!extractPath.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                extractPath += Path.DirectorySeparatorChar;
+            }
+
+            using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+            {
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    if (entry.FullName.EndsWith(".exe", StringComparison.OrdinalIgnoreCase))
+                    {
+                        // Gets the full path to ensure that relative segments are removed.
+                        string destinationPath = Path.GetFullPath(Path.Combine(extractPath, entry.FullName));
+
+                        // Ordinal match is safest, case-sensitive volumes can be mounted within volumes that
+                        // are case-insensitive.
+                        if (destinationPath.StartsWith(extractPath, StringComparison.Ordinal))
+                            entry.ExtractToFile(destinationPath);
+                    }
+                }
+            }
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            lblRetaguarda.Parent = pictureBox1;
+            lblStatus.Parent = pictureBox1;
+            lblPdv.Parent = pictureBox1;
+            label1.Parent = pictureBox1;
+
+            lblRetaguarda.BackColor = Color.Transparent;
+            lblStatus.BackColor = Color.Transparent;
+            lblPdv.BackColor = Color.Transparent;
+            label1.BackColor = Color.Transparent;
+
+            lblPdv.Text = OutDatedPdv() ? "Pdv: Desatualizado" : "Pdv: Atualizado";
+
+            lblRetaguarda.Text = OutDatedFusion() ? "Retaguarda: Desatualizado" : "Retaguarda: Atualizado";
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            string statusPdv = lblPdv.Text;
+            string statusFusion = lblRetaguarda.Text;
+            if (statusPdv.Substring(5).Equals("Desatualizado"))
+            {
+                PdvDownload();
+            }
+
+            if (statusFusion.Substring(12).Equals("Desatualizado"))
+            {
+                FusionDownload();
+            }
+        }
+    }
+}
